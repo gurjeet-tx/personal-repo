abab7d90cb161c3cda21c8535eb2711bc6d8faa4
gurjeet-tx <gurjeet.singh@testingxperts.com>
2021-02-18 16:17:38 +0530

    python file from local machine

---
+++ python2.py
@@ -0,0 +1,34 @@
+def find_file(filename, std_dirs, paths):
+    """Searches for the directory where a given file is located,
+    and returns a possibly-empty list of additional directories, or None
+    if the file couldn't be found at all.
+    'filename' is the name of a file, such as readline.h or libcrypto.a.
+    'std_dirs' is the list of standard system directories; if the
+        file is found in one of them, no additional directives are needed.
+    'paths' is a list of additional locations to check; if the file is
+        found in one of them, the resulting list will contain the directory.
+    """
+    if MACOS:
+        # Honor the MacOSX SDK setting when one was specified.
+        # An SDK is a directory with the same structure as a real
+        # system, but with only header files and libraries.
+        sysroot = macosx_sdk_root()
+
+    # Check the standard locations
+    for dir in std_dirs:
+        f = os.path.join(dir, filename)
+
+        if MACOS and is_macosx_sdk_path(dir):
+            f = os.path.join(sysroot, dir[1:], filename)
+
+        if os.path.exists(f): return []
+
+    # Check the additional directories
+    for dir in paths:
+        f = os.path.join(dir, filename)
+
+        if MACOS and is_macosx_sdk_path(dir):
+            f = os.path.join(sysroot, dir[1:], filename)
+
+        if os.path.exists(f):
+            return [dir]
